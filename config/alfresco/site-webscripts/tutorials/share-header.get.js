28255ab335153161b6fc4513d045c59801eb0938
uk-ar <yuuki.ari@gmail.com>
2014-09-12 05:28:17 +0900

    replace site recent menu

--- config/alfresco/site-webscripts/tutorials/share-header.get.js
+++ config/alfresco/site-webscripts/tutorials/share-header.get.js
@@ -124,3 +124,11 @@ if (adminMenu != null)
       }
    ];
 }
+//Find the "Sites" menu...
+   var sitesMenu = 
+     widgetUtils.findObject(model.jsonModel, "id", "HEADER_SITES_MENU");
+   if (sitesMenu != null)
+   {
+     // Change the widget to our custom menu...
+     sitesMenu.name = "tutorials/sites-menu";
+   }
